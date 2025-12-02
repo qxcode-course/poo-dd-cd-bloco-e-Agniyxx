e55ea5294b58c802fe14e65213748d3e89613f5f
Agne <agne.steph.moss@gmail.com>
2025-12-02 13:24:43 -0300

    iniciando atividade zoo

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -1,5 +1,14 @@
 class Animal:
-    def __init__(self, nome: str):
-        self.__nome = nome
+    nome: str
 
+    def __init__(self, nome: str):
+        self.nome = nome
+
+    def apresentar_nome(self):
+        print("Eu sou um(a) {self.nome}!")
     
+    def fazerSom(self):
+
+    def mover(self):
+
+        
