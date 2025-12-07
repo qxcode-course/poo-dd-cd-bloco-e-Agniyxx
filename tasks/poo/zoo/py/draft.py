aeb80630e89537a8bc8e19f8a71ed93d38193413
Agne <agne.steph.moss@gmail.com>
2025-12-07 10:49:07 -0300

    corrigindo zoo

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -17,11 +17,48 @@ class Lontra(Animal):
     def __init__(self, nome):
         super().__init__(nome)
         
+    def fazerSom(self):
+        return super().fazerSom()
+    
+    def mover(self):
+        return super().mover()
+
+class Furao(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+    
+    def fazerSom(self):
+        return super().fazerSom()
+    
+    def mover(self):
+        return super().mover()
+    
+class Rato(Animal):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+    def fazerSom(self):
+        return super().fazerSom()
+    
+    def mover(self):
+        return super().mover()
 
 def apresentar(animal: Animal):
-    
+    animal.apresentar_nome()
+    animal.fazerSom()
+    animal.mover()
+
+    print("{type(animal).__name__}")
 
 
+zoo: list[Animal] = [
+    Lontra("Otto")
+    Furao("Pan")
+    Rato("Stuart")
+    ]
 
+print("# teste")
+print()
 
-        
+for animal_inst in zoo:
+    apresentar(animal_inst)
